d2c12b0e00a053304f6750194589645939e37710
DeusBlu <DeusBlu@Gmail.com>
2015-06-09 00:32:09 -0700

    OpenGL Window working

---
+++ ProjectFiles/qtSandboxGame/MyGlWindow.h
@@ -0,0 +1,10 @@
+#ifndef MYGLWINDOW_H
+#define MYGLWINDOW_H
+#include <QtOpenGL\qgl.h>
+#pragma once
+class MyGlWindow : public QGLWidget
+{
+public:
+};
+
+#endif //MYGLWINDOW_H
--- ProjectFiles/qtSandboxGame/main.cpp
+++ ProjectFiles/qtSandboxGame/main.cpp
@@ -1,9 +1,16 @@
 
-#include <QtCore/QCoreApplication>
+#include <QtWidgets/qapplication.h>
+//#include <QtWidgets\qwidget.h>
+#include "MyGlWindow.h"
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication a(argc, argv);
+	QApplication a(argc, argv);
 
+	//QWidget myWidget;
+	//myWidget.show();
+
+	MyGlWindow myGlWindow;
+	myGlWindow.show();
 	return a.exec();
 }
